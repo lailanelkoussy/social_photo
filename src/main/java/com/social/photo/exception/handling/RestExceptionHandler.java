323c1a9cd467f4437bdb2b050fc46a386fc5dbe1
lailanelkoussy <lailanelkoussy@aucegypt.edu>
2019-07-14 10:38:44 +0200

    Simplified exception handling

---
+++ src/main/java/com/social/photo/exception/handling/RestExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.social.photo.exception.handling;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.persistence.EntityNotFoundException;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+@Slf4j
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(javax.validation.ConstraintViolationException.class)
+    protected ResponseEntity<Object> handleConstraintViolation(
+            javax.validation.ConstraintViolationException ex) {
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    protected ResponseEntity<Object> handleEntityNotFound(
+            EntityNotFoundException ex) {
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+    }
+
+    @ExceptionHandler(IllegalAccessException.class)
+    protected ResponseEntity<Object> handleIllegalAccessException(IllegalAccessException ex) {
+        return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+    }
+
+}
+
